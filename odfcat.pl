d9e13ce3ddfe7b7166648282146342a3de011d03
Wyatt <wpillmore@gmail.com>
2012-02-19 19:29:41 -0700

    Updated base

--- odfcat.pl
+++ odfcat.pl
@@ -1,35 +1,38 @@
 #!/usr/bin/perl
 use warnings; 
-use strict;
 use XML::Simple;
 use Data::Dumper;
 use Getopt::Long;
 
 $|++;
 my $file = $ARGV[0];
-print '::Fetching - ' . $file . "\n";
+print 'odfcat::Fetching - ' . $file . "\n";
 &init();
 					  	 
 sub init {
 	qx(if [ ! -f $file ];
 		then 
-			echo '$0 :: File not found!';
+			echo 'odfcat :: File not found!';
 		else
-			echo '$0 :: Extracting contents ... ';
-			if [ ! -d ~/.odfcat ];
+			echo odfcat :: Extracting contents ... 
+			if [ ! -d /tmp/.odfcat ];
 			 then
-				mkdir ~/.odfcat;
-				echo '$0 :: Unzipping contents ... ';
-				unzip $file -d ~/.odfcat;
-				chmod 777 -R ~/.odfcat;
+				mkdir /tmp/.odfcat;
+				echo odfcat :: Unzipping contents ... 
+				unzip $file -d /tmp/.odfcat/;
 			fi;
-			echo '$0 :: Unzipping contents ... ';
-			unzip $file -d ~/.odfcat;
 			
+			echo odfcat:: Unzipping contents ... 
+			unzip $file -d /tmp/.odfcat;
 		fi;);
+		
 	my %xml = \&main();
-	print %xml;
-	
+	for my $keys (keys %xml) {
+		print "$keys -> $xml{$keys}\n";
+	}
+	qx(rm -rf /home/.odfcat;);
+	print "odfcat :: Finished!\n";
+	exit(0);
 }
 
 sub tolog {
@@ -41,15 +44,16 @@ sub help {
 }
 
 sub main {
-	my $xml = XMLin("~/.odfcat/meta.xml");
-	my %xml = ( "version" => "$xml->{'office:meta'}->{'meta:generator'} " . $xml->{'office:version'},
-			 "creator " => $xml->{'office:meta'}->{'meta:initial-creator'},
-			 "cdate" => $xml->{'office:meta'}->{'dc:date'},
-			 "edited" => $xml->{'office:meta'}->{'meta:editing-duration'},
-			 "wcount" => $xml->{'office:meta'}->{'meta:document-statistic'}->{'meta:word-count'},
-			 "pcount" => $xml->{'office:meta'}->{'meta:document-statistic'}->{'meta:page-count'},
-			 "ccount" => $xml->{'office:meta'}->{'meta:document-statistic'}->{'meta:character-count'},
-			 "content" => $xml->{'office:body'}->{'office:text'}->{'text:p'}->{content},
-	);
-	return %xml;
+	my $file = "/tmp/.odfcat/meta.xml";
+	my $xml = XMLin($file);	
+	my %xmlin = ("version" => $xml->{'office:meta'}->{'meta:generator'} . $xml->{'office:version'},
+			   "creator"  => $xml->{'office:meta'}->{'meta:initial-creator'},
+			   "cdate" => $xml->{'office:meta'}->{'dc:date'},
+			   "edited" => $xml->{'office:meta'}->{'meta:editing-duration'},
+			   "wcount" => $xml->{'office:meta'}->{'meta:document-statistic'}->{'meta:word-count'},
+			   "pcount" => $xml->{'office:meta'}->{'meta:document-statistic'}->{'meta:page-count'},
+			   "ccount" => $xml->{'office:meta'}->{'meta:document-statistic'}->{'meta:character-count'},
+			   "content" => $xml->{'office:body'}->{'office:text'}->{'text:p'}->{content},
+			);
+	return $xmlin;
 }
