98e56e3294258e523599d164f7696f4107d9621b
Wyatt <wpillmore@gmail.com>
2012-02-19 22:18:59 -0700

    Created perldoc --man/--help options using Pod::Usage + small changes to the hierarchy to make sure your help and man calls come first.

--- odfcat.pl
+++ odfcat.pl
@@ -1,21 +1,28 @@
-#!/usr/bin/perl
+#!/usr/local/bin/perl
 use warnings; 
 use XML::Simple;
 use Getopt::Long;
+use Pod::Usage;
 use Switch;
 
 $|++;
 our $verbose = 0;
 our $result = GetOptions ("quiet" => \$verbose,
-			  "verbose" => sub{$verbose = 1;},
-			  "help" => sub{$verbose = 2;});
+			  		 	  "verbose" => sub{$verbose = 1;},
+			  			  "help" => sub{$verbose = 2;},
+			  			  "man" => sub{$verbose = 3;});
 						  
-my $file = $ARGV[0];
-print 'odfcat::Fetching - ' . $file . "\n";
-&init();
+our $file = $ARGV[0];
+if($verbose > 1) {
+		&help();
+}
+else {
+	&init();
+}
 
 # ------------ Fetches file and checks to make sure it exists, then Unzips it using unzip utility and extracts to temporary directory ---------------- #
 sub init {
+	print $0 . ' ::Fetching - ' . $file . "\n";
 	qx(if [ ! -f $file ];
 		then 
 			echo odfcat :: File not found!;
@@ -30,21 +37,22 @@ sub init {
 		fi;);
 	
 # ------------ Checks verbose setting as set by our flags and calls appropriate subroutine ------------- #
-	if($verbose == 2) {
-		&help();
-	}
-	else {
-		&main();
-	}
+
+	&main();
 # ------------ Cleanup and exit ------------- #	
-	print "\nodfcat :: Finished!\n";
+	print "\n$0 :: Finished!\n";
 	qx(/bin/rm -rf /tmp/.odfcat;);
 	exit(0);
 }
 
 # ------------- TODO: Getopts help and how to use. ------------- #
 sub help {
-	
+	if ($verbose == 2) {
+	Pod::Usage::pod2usage();
+	}
+	else {
+		Pod::Usage::pod2usage(-verbose => 2);
+	}
 }
 
 # ------------- Uses XML::Simple on the two main content and information schema files in odf to parse necessary information and print - how much depending on verbosity -------------- #
@@ -78,3 +86,43 @@ sub main {
 		else {return};
 	}
 }
+__END__
+
+=head1 NAME
+
+Odfcat - simple ODF parsing tool to display necessary information from Open Document files.
+
+=head1 SYNOPSIS
+
+Use:
+
+    odfcat [--help/man] 
+    odfcat [--verbose/-v] [file.odt]
+
+Examples:
+
+    odfcat -v [file] | odfcat --verbose [file.odt]
+    odfcat --help | --man
+    
+    Default quiet verbosity.
+
+=head1 DESCRIPTION
+
+This script uses the XML::Simple module to parse Open Document files for basic information associated with user, word count, page count, version, edit dates, and other criteria. It is designed to be used in the same way you would 'cat' a file, and content is readily displayed if necessary.
+
+=back
+
+=head1 AUTHOR
+
+Wyatt Pillmore, E<lt>wpillmore@gmail.comE<gt>.
+
+=head1 COPYRIGHT
+
+This program is distributed under the SHUT UP AND TAKE MY MONEY license.
+
+=head1 DATE
+
+19-2-2012
+
+=cut
+
