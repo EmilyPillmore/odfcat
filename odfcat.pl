4f23b27b73992c00bfee2f70d6d356ca12999eed
Wyatt <wpillmore@gmail.com>
2012-02-23 22:17:54 -0700

    Little cleanups here and there. Some undef to be a good citizen.

--- odfcat.pl
+++ odfcat.pl
@@ -7,37 +7,36 @@ use Pod::Usage;
 
 $|++;
 my $verbose;
-my ($help, $man);
-Getopt::Long::GetOptions ("quiet" => \$verbose,
-			  "verbose" => sub{$verbose = 1;},
-			  "help" => \$help,
-			  "man" => \$man);
+Getopt::Long::GetOptions ( "quiet" => \$verbose,
+						   "verbose" => sub{ $verbose = 1; },
+						   "help" => \my $help,
+						   "man" => \my $man );
 						  			
-Pod::Usage::pod2usage( -verbose => 1 ) if ($help);
-Pod::Usage::pod2usage( -verbose => 2 ) if ($man);	  
+Pod::Usage::pod2usage( -verbose => 1 ) if $help;
+Pod::Usage::pod2usage( -verbose => 2 ) if $man;	  
 
 # Fetches file and checks to make sure it exists, then Unzips it using unzip utility and extracts to temporary directory
 @ARGV or Pod::Usage::pod2usage( -verbose => 1 );
 my $file = $ARGV[0];
-print $0 . ' :: Fetching - ' . $file . "\n";
-	
+
+print "$0 :: Fetching - $file\n";
 qx(if [ ! -f $file ];
-    then 
-	echo "$0 :: File not found!"
-    else
-	if [ ! -d /tmp/.odfcat/ ];
-	 then
-		mkdir /tmp/.odfcat/
-		unzip $file -d /tmp/.odfcat/
-	fi	
-	unzip $file -d /tmp/.odfcat/
-   fi);
-	
+	then 
+		echo "$0 :: File not found!"
+	else
+		if [ ! -d /tmp/.odfcat/ ];
+		 then
+			mkdir /tmp/.odfcat/
+			unzip $file -d /tmp/.odfcat/
+		fi	
+			unzip $file -d /tmp/.odfcat/
+	fi);
+
 main();
 	
 # Cleanup and exit
 print "\n$0 :: Finished!\n";
-qx(/bin/rm -rf /tmp/.odfcat);
+qx(/bin/rm -rf /tmp/.odfcat/);
 exit 0;
 
 # Uses XML::Simple on the two main content and information schema files in odf to parse necessary information and print - how much depending on verbosity
@@ -45,7 +44,7 @@ sub main {
 	my $xml = XMLin("/tmp/.odfcat/meta.xml");	
 	my $content = XMLin("/tmp/.odfcat/content.xml");
 	
-	if ($verbose == 0) {
+	if ( ! $verbose == 1) {
 		print "\nVersion :: " . $xml->{'office:version'} . "\n";
 		print "Creator :: " . $xml->{'office:meta'}->{'meta:initial-creator'} . "\n";
 		print "Creation Date :: " . $xml->{'office:meta'}->{'dc:date'} . "\n";
@@ -61,7 +60,7 @@ sub main {
 		print "Page Count :: " . $xml->{'office:meta'}->{'meta:document-statistic'}->{'meta:page-count'} . "\n";
 		print "Character Count :: " . $xml->{'office:meta'}->{'meta:document-statistic'}->{'meta:character-count'} . "\n";
 		
-# Content printing is available, but not recommended for large documents		
+# Content printing is available, but not recommended for large documents 
 		print "Print content? [Y/n]: ";
 		chomp(my $in = <STDIN>);
 		if ($in eq "Y") {
@@ -69,6 +68,8 @@ sub main {
 		}
 		else {return};
 	}
+#cleanup of unnecessary local variables
+	undef $xml, $content, $verbose;
 }
 __END__
 
@@ -80,8 +81,8 @@ Odfcat - simple ODF parsing tool to display necessary information from Open Docu
 
 Use:
 
-    odfcat [--help/man] 
-    odfcat [--verbose/-v] [file.odt]
+    odfcat [--help|man] 
+    odfcat [--verbose|-v] [file.odt]
 
 Examples:
 
