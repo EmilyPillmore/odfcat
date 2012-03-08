275cc572d07677f130bde202a446ddf504a5afb5
Wyatt <wpillmore@gmail.com>
2012-03-07 20:59:53 -0700

    First version of updated OdfCat that allows writefile options and output as XML. Also, updated syntax for clarity.

--- odfcat.pl
+++ odfcat.pl
@@ -8,26 +8,30 @@ use Pod::Usage;
 $|++;
 my $verbose;
 Getopt::Long::GetOptions ( "quiet" => \$verbose,
-			   "verbose" => sub{ $verbose = 1; },
-			   "help" => my $help,
-			   "man" => my $man );
+		  	   "verbose" => sub{ $verbose = 1; },
+			   "file=s" => my $output,
+			   "as-xml" => my $flag,
+			   "help" => \my $help,
+			   "man" => \my $man );
 						  			
 Pod::Usage::pod2usage( -verbose => 1 ) if $help;
-Pod::Usage::pod2usage( -verbose => 2 ) if $man;	  
+Pod::Usage::pod2usage( -verbose => 2 ) if $man;
 
 # Fetches file and checks to make sure it exists, then Unzips it using unzip utility and extracts to temporary directory
 @ARGV or Pod::Usage::pod2usage( -verbose => 1 );
 my $file = $ARGV[0];
 
-! -e $file 
- ? die "$0 :: File not found!"
- : print "$0 :: extracting - $file\n",
- ! -d '/tmp/.odfcat'
- 	? qx(mkdir /tmp/.odfcat/ &&
-	  /usr/bin/unzip $file -d '/tmp/.odfcat/')
-	: qx(/usr/bin/unzip $file -d '/tmp/.odfcat/');
-
-main();
+if(! -e $file) {
+  	die "$0 :: File not found!"
+}
+else {
+	print "$0 :: extracting - $file\n";
+ 	! -d '/tmp/.odfcat'
+ 		? qx(mkdir /tmp/.odfcat/ &&
+	  	unzip $file -d '/tmp/.odfcat/')
+		: qx(unzip $file -d '/tmp/.odfcat/');
+}
+$flag ? as_xml() : main();
 	
 # Cleanup and exit
 print "\n$0 :: Finished!\n";
@@ -39,18 +43,42 @@ sub main {
 	my $xml = XMLin("/tmp/.odfcat/meta.xml");	
 	my $content = XMLin("/tmp/.odfcat/content.xml");
 	
-        !$verbose == 1
-		? return print "\nVersion :: $xml->{'office:version'}\nCreator :: $xml->{'office:meta'}->{'meta:initial-creator'}\nCreation Date :: $xml->{'office:meta'}->{'dc:date'}\nLast Edited :: $xml->{'office:meta'}->{'meta:editing-duration'}\nWord Count :: $xml->{'office:meta'}->{'meta:document-statistic'}->{'meta:word-count'}\n"
-		: print "\nVersion :: $xml->{'office:version'}\nCreator :: $xml->{'office:meta'}->{'meta:initial-creator'}\nCreation Date :: $xml->{'office:meta'}->{'dc:date'}\nLast Edited :: $xml->{'office:meta'}->{'meta:editing-duration'}\nWord Count :: $xml->{'office:meta'}->{'meta:document-statistic'}->{'meta:word-count'}\nPage Count :: $xml->{'office:meta'}->{'meta:document-statistic'}->{'meta:page-count'}\nCharacter Count :: $xml->{'office:meta'}->{'meta:document-statistic'}->{'meta:character-count'}\n";
-		
-# Content printing is available, but not recommended for large documents 
-	print "\nPrint content? [Y/n]: ";
-	<STDIN> =~ /^y\n/i
-		? print "Printing Content :: \n\t$content->{'office:body'}->{'office:text'}->{'text:p'}->{'content'}\n"
-		: return;
-
-	undef $xml, $content, $verbose;
+    if($output) {
+    	open(OUTFILE, '>', $output) or die "$0 :: Could not open file. Does it exist, or does it not allow Write Access?";
+    	! $verbose == 1
+		? print OUTFILE "\nVersion :: $xml->{'office:version'}\nCreator :: $xml->{'office:meta'}->{'meta:initial-creator'}\nCreation Date :: $xml->{'office:meta'}->{'dc:date'}\nLast Edited :: $xml->{'office:meta'}->{'meta:editing-duration'}\nWord Count :: $xml->{'office:meta'}->{'meta:document-statistic'}->{'meta:word-count'}\n"
+		: print OUTFILE "\nVersion :: $xml->{'office:version'}\nCreator :: $xml->{'office:meta'}->{'meta:initial-creator'}\nCreation Date :: $xml->{'office:meta'}->{'dc:date'}\nLast Edited :: $xml->{'office:meta'}->{'meta:editing-duration'}\nWord Count :: $xml->{'office:meta'}->{'meta:document-statistic'}->{'meta:word-count'}\nPage Count :: $xml->{'office:meta'}->{'meta:document-statistic'}->{'meta:page-count'}\nCharacter Count :: $xml->{'office:meta'}->{'meta:document-statistic'}->{'meta:character-count'}\n";
+	close OUTFILE;
+	return;
+	}
+	if( ! $verbose == 1) { 
+		return print "\nVersion :: $xml->{'office:version'}\nCreator :: $xml->{'office:meta'}->{'meta:initial-creator'}\nCreation Date :: $xml->{'office:meta'}->{'dc:date'}\nLast Edited :: $xml->{'office:meta'}->{'meta:editing-duration'}\nWord Count :: $xml->{'office:meta'}->{'meta:document-statistic'}->{'meta:word-count'}\n";
+	}
+	else {
+		print "\nVersion :: $xml->{'office:version'}\nCreator :: $xml->{'office:meta'}->{'meta:initial-creator'}\nCreation Date :: $xml->{'office:meta'}->{'dc:date'}\nLast Edited :: $xml->{'office:meta'}->{'meta:editing-duration'}\nWord Count :: $xml->{'office:meta'}->{'meta:document-statistic'}->{'meta:word-count'}\nPage Count :: $xml->{'office:meta'}->{'meta:document-statistic'}->{'meta:page-count'}\nCharacter Count :: $xml->{'office:meta'}->{'meta:document-statistic'}->{'meta:character-count'}\n";
+		print "\nPrint content? [Y/n]: ";
+		<STDIN> =~ /^y\n/i
+			? print "Printing Content :: \n\t$content->{'office:body'}->{'office:text'}->{'text:p'}->{'content'}\n"
+			: return;
+	}
+	#cleanup of unnecessary local variables
+	undef $xml, $content, $verbose, $output;
+	return;
 }
+
+sub as_xml { 
+	die "Please supply a file to write to if you want XML output." unless $output;
+	
+	my $xml = XMLin("/tmp/.odfcat/meta.xml");	
+	my $content = XMLin("/tmp/.odfcat/content.xml");
+	
+	open(OUTFILE, '>', $output);
+		print OUTFILE $xml;
+		print OUTFILE $content;
+	close OUTFILE;
+	return;
+}	
+
 __END__
 
 =head1 NAME
